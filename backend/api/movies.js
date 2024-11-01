5ba2c017d8765d6b2ca4dccfce8d9f687be34da6
Ayomide Aremu-Cole <121998651+ayomidecole@users.noreply.github.com>
2024-11-01 04:00:42 -0400

    womp womp

--- backend/api/movies.js
+++ backend/api/movies.js
@@ -1,7 +1,8 @@
 import express from "express"
+import MoviesController from "./controller.js"
 
 const router = express.Router()
 
-router.route("/").get((req,res) => res.send('hello world'))
+router.route("/").get(MoviesController.apiGetMovies)
 
 export default router
